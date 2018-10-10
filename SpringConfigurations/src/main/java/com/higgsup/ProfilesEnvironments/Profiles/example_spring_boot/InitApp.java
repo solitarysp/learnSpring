25c5d14e15081f9992bf9071ae657a2c54de4a7d
Chi Can Em <lethanh9398@gmail.com>
2018-10-11 00:23:39 +0700

    example

--- SpringConfigurations/src/main/java/com/higgsup/ProfilesEnvironments/Profiles/example_spring_boot/InitApp.java
+++ SpringConfigurations/src/main/java/com/higgsup/ProfilesEnvironments/Profiles/example_spring_boot/InitApp.java
@@ -3,6 +3,7 @@ package com.higgsup.ProfilesEnvironments.Profiles.example_spring_boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
